04b6bd6da56aa4f4af0c37da1b90477230af8aec
Futr <101582610+GoFutr@users.noreply.github.com>
2023-05-27 14:52:17 +0100

    Create v0.1.py

---
+++ v0/v0.1/v0.1.py
@@ -0,0 +1,125 @@
+#(c) FutrOS 2020. All rights reserved.
+#Last updated: 05/26/2023
+#It is illegal to redistribute or rebrand this software without Author permission.
+
+import os
+
+print ("""
+                                                                ███████╗██╗░░░██╗████████╗██████╗░  ░█████╗░░██████╗
+                                                                ██╔════╝██║░░░██║╚══██╔══╝██╔══██╗  ██╔══██╗██╔════╝
+                                                                █████╗░░██║░░░██║░░░██║░░░██████╔╝  ██║░░██║╚█████╗░
+                                                                ██╔══╝░░██║░░░██║░░░██║░░░██╔══██╗  ██║░░██║░╚═══██╗
+                                                                ██║░░░░░╚██████╔╝░░░██║░░░██║░░██║  ╚█████╔╝██████╔╝
+                                                                ╚═╝░░░░░░╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝  ░╚════╝░╚═════╝░   BETA 0.1
+                                    [⚠]: This Version Of FutrOS is listed under "not supported", update to a newer version for full software support
+""")
+
+print ("""
+                                                                                [1] About [2] Apps \n©2020 Futr. All rights Reserved.""")
+setup = input ("[?]:")
+
+if setup ==  '1':
+        print ("""   
+                                                                              ╔═══╦╗───────╔╗     
+                                                                              ║╔═╗║║──────╔╝╚╗
+                                                                              ║║─║║╚═╦══╦╗╠╗╔╝
+                                                                              ║╚═╝║╔╗║╔╗║║║║║S
+                                                                              ║╔═╗║╚╝║╚╝║╚╝║╚╗
+                                                                              ╚╝─╚╩══╩══╩══╩═╝
+
+                                                                ███████╗██╗░░░██╗████████╗██████╗░  ░█████╗░░██████╗
+                                                                ██╔════╝██║░░░██║╚══██╔══╝██╔══██╗  ██╔══██╗██╔════╝
+                                                                █████╗░░██║░░░██║░░░██║░░░██████╔╝  ██║░░██║╚█████╗░
+                                                                ██╔══╝░░██║░░░██║░░░██║░░░██╔══██╗  ██║░░██║░╚═══██╗
+                                                                ██║░░░░░╚██████╔╝░░░██║░░░██║░░██║  ╚█████╔╝██████╔╝
+                                                                ╚═╝░░░░░░╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝  ░╚════╝░╚═════╝░
+
+                                                                      Version 0.1 Public Beta Preview 
+                                                          ©2020 Futr. This is a licensed and authentic version of FutrOS.  
+                                        [⚠]: This version is listed under "not supported". Update to the latest version for full software support.
+
+
+    """)
+
+if setup == '2':
+        print(f"""
+        
+                                                                                  ╔═══╗
+                                                                                  ║╔═╗║
+                                                                                  ║║─║╠══╦══╦══╗
+                                                                                  ║╚═╝║╔╗║╔╗║══╣
+                                                                                  ║╔═╗║╚╝║╚╝╠══║
+                                                                                  ╚╝─╚╣╔═╣╔═╩══╝
+                                                                                  ────║║─║║
+                                                                                  ────╚╝─╚╝
+                                                                                    [1] Insert App Name Here
+                                                        
+        """)
+        
+
+if setup ==  'awesome':
+        print ("""
+
+╔══╦╗──────╔╗────────╔╗──────────────────────────╔╗───╔╗─────────╔╗
+╚╗╔╣╚╦═╗╔═╦╣╠╗╔╦╦═╦╦╗║╚╦═╗╔═╦═╦═╦═╦╦╦╦╦═╦═╦╦═╗╔╦╦╣╚╦═╗║╚╦═╦╗╔═╦═╦╝║
+─║║║║║╬╚╣║║║═╣║║║╬║║║║╔╣╬║║╩╬╗║╔╣╩╣╔╣║║╬║║║║╩╣║║║║║║╬║║║║╩╣╚╣╬║╩╣╬║
+─╚╝╚╩╩══╩╩═╩╩╝╠╗╠═╩═╝╚═╩═╝╚═╝╚═╝╚═╩╝╠╗╠═╩╩═╩═╝╚══╩╩╩═╝╚╩╩═╩═╣╔╩═╩═╝
+──────────────╚═╝───────────────────╚═╝─────────────────────╚╝
+─────╔╗──╔══╗╔╗──╔═╦══╗
+╔═╦═╦╝╠═╗║═╦╬╣╚╦╦╣║║══╣
+║═╣╬║╬║╩╣║╔╣║║╔╣╔╣║╠══║
+╚═╩═╩═╩═╝╚╝╚═╩═╩╝╚═╩══╝
+
+               """)
+if setup == 'eos':
+       print ("""
+       This Version Of FutrOS is listed under "not supported", update to a newer version for full software support.
+       """)
+if setup == 'routing':
+      print ("""
+      FutrOS Manual Routing.
+      """)
+if setup == 'v0':
+      print ("""
+      Thanks for using FutrOS, we hope you enjoy using v0.1!
+      """)
+if setup == 'codename':
+    print("""
+      V1S
+    """)
+if setup == 'c':
+        print("""
+        (c) 2020 Futr. All rights reserved.
+        """)
+if setup =='switchverlocation':
+        print("""
+        Version specific location switching isn't available on this device.
+        """)
+if setup == 'upgrade':
+      print ("""
+      Upgrade to newer versions of FutrOS at https://bit.ly/futrosupdate.
+      """)
+if setup ==  'beta':
+        print ("""         
+This is a beta version of FutrOS
+               """)
+if setup == 'archive':
+        print("""
+This version of FutrOS is archived
+        """)
+if setup ==  'help':
+        print ("""
+All commands:
+eos - End of Support Info
+Routing - Routing Info
+c - Copyright Info for FutrOS 0.1
+Upgrade - How to upgrade FutrOS
+beta - Beta Info
+Archive - OS Archive Info
+
+Commands are case-sensitive. """)
+
+#Import Apps Here (using define and myapp1)
+#e.g. 
+# def app1():
+#   print("My Cool App!")
